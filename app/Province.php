d64c9e9cae840cd6498e7b98b3a22d58353dedee
Necrobone <necrobone@hotmail.com>
2020-01-02 16:42:44 +0100

    Province and some refactor

--- app/Province.php
+++ app/Province.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Suplier extends Model
+class Province extends Model
 {
     //
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,10 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('api_token', 80)->unique()->nullable()->default(null);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
